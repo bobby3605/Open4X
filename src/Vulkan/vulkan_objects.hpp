75ddc71fb6e3992675e4592c110dc6ac62be026a
bobby3605 <sonic921150@gmail.com>
2022-12-22 16:54:09 -0600

    Trying to fix the partial sums for instanceCount.

--- src/Vulkan/vulkan_objects.hpp
+++ src/Vulkan/vulkan_objects.hpp
@@ -28,6 +28,7 @@ class VulkanObjects {
     std::shared_ptr<VulkanBuffer> visibilityBuffer;
     std::shared_ptr<VulkanBuffer> blockCountsBuffer;
     std::shared_ptr<VulkanBuffer> prefixSumBuffer;
+    std::shared_ptr<VulkanBuffer> partialSumBuffer;
     std::shared_ptr<SSBOBuffers> ssboBuffers;
     std::vector<std::shared_ptr<VulkanObject>> objects;
     std::vector<std::future<std::shared_ptr<VulkanObject>>> futureObjects;
