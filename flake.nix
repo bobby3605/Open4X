5901e9bc68a14416b0f4d2808fece6824e7e4fb7
bobby3605 <sonic921150@gmail.com>
2024-08-05 18:19:40 -0500

    nix flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Open4X nixos flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+  };
+
+  outputs = { self , nixpkgs ,... }: let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+	glm
+	vulkan-headers
+	vulkan-loader
+	vulkan-validation-layers
+	glfw
+	glslang
+      ];
+      shellHook = ''
+        exec zsh
+      '';
+
+    };
+  };
+}
